adc0f60a7fe3d96dbdcdf842e4a3500995aa536d
jonlee48 <jonathan.lee48@gmail.com>
2025-02-25 17:43:43 -0500

    subgraph seems to be working?

--- torchviz/dot.py
+++ torchviz/dot.py
@@ -1,5 +1,6 @@
 from graphviz import Digraph
 import torch
+from copy import copy
 from torch.autograd import Variable
 
 from .wrapper import grad_fn_metadata, Metadata
@@ -39,7 +40,7 @@ def get_fn_name(fn, max_attr_chars=50):
     return name + '\n' + sep + '\n' + params
 
 
-def make_dot(var, params=None):
+def make_dot(var, params=None, hide_no_ops=True):
     """ Produces Graphviz representation of PyTorch autograd graph.
 
     If a node represents a backward function, it is gray. Otherwise, the node
@@ -51,8 +52,6 @@ def make_dot(var, params=None):
          a dark green node.
     """
     global grad_fn_metadata
-    print("IN MAKE DOT")
-    print(grad_fn_metadata)
 
     if params is not None:
         assert all(isinstance(p, Variable) for p in params.values())
@@ -68,7 +67,10 @@ def make_dot(var, params=None):
                      height='0.2',
                      fontname='monospace')
     dot = Digraph(node_attr=node_attr, graph_attr=dict(size="12,12"))
+    print("ID DOT")
+    print(id(dot))
     seen = set()
+    all_subgraphs = {}
 
     def size_to_str(size):
         return "(" + (", ").join([str(v) for v in size]) + ")"
@@ -78,8 +80,17 @@ def make_dot(var, params=None):
             name = param_map[id(var)] if id(var) in param_map else ''
         return f"{name}\n{size_to_str(var.size())}"
 
-    def add_nodes(fn, hide_no_ops=True):
-        print(f"add_node {id(fn)} {fn}")
+    def add_node_to_subgraphs(str_id, text, fillcolor='lightgray', subgraphs=[]):
+        print("iterating over subgraphs")
+        for sub in subgraphs:
+            print(f"adding node to {id(sub)}")
+            sub.node(str_id, text, fillcolor=fillcolor)
+        
+        for i in range(len(subgraphs) - 1):
+            subgraphs[i].subgraph(subgraphs[i+1])
+
+
+    def add_nodes(fn, subgraphs=[dot]):
         assert not torch.is_tensor(fn)
         if fn in seen:
             return
@@ -88,37 +99,62 @@ def make_dot(var, params=None):
         if hasattr(fn, 'variable'):
             # if grad_accumulator, add the node for `.variable`
             var = fn.variable
-            seen.add(var)
             dot.node(str(id(var)), get_var_name(var), fillcolor='lightblue')
             dot.edge(str(id(var)), str(id(fn)))
+            seen.add(var)
 
         # add the node for this grad_fn
-        # if not skip:
         if id(fn) in grad_fn_metadata:
             meta = grad_fn_metadata[id(fn)]
-            dot.node(str(id(fn)), get_fn_name(fn), fillcolor=meta.color)
+
+            # assume for each function call the group is unique
+            if meta.is_ret:
+                print("is ret")
+                if meta.group not in all_subgraphs:
+                    print(f"creating new subgraph for {meta.group}")
+                    sub = Digraph(name=f"cluster_{meta.group}")
+                    sub.attr(label=meta.group, style="dotted", color="blue")
+                    all_subgraphs[meta.group] = sub
+                subgraphs.append(all_subgraphs[meta.group])
+
+            # dot.node(str(id(fn)), get_fn_name(fn), fillcolor=meta.color)
+            add_node_to_subgraphs(str(id(fn)), get_fn_name(fn), fillcolor=meta.color, subgraphs=subgraphs)
+
+            if meta.is_arg:
+                print("IS ARG")
+                print(subgraphs)
+                # subgraphs.remove(all_subgraphs[meta.group])
+                sub = subgraphs.pop()
+                # if len(subgraphs) >= 1:
+                subgraphs[-1].subgraph(sub)
+                # else:
+                    # dot.subgraph(sub)
+
             if hide_no_ops and meta.no_op:
                 # no_ops are created when we add a label_val() to a tensor that
                 # has no grad_fn, so we introduce a no_op grad_fn so the label
                 # appears in the computation graph
 
-                # skip over the no_op and draw edges to the no_op's next_function
+                # skip over the no_op and recurse
                 next_fn = fn.next_functions[0][0]
                 for v in next_fn.next_functions:
                     if v[0] is not None:
                         dot.edge(str(id(v[0])), str(id(fn)))
-                        add_nodes(v[0])
+                        # add_nodes(v[0])
+                        add_nodes(v[0], copy(subgraphs))
                 return
 
         else: 
-            dot.node(str(id(fn)), get_fn_name(fn))
+            # dot.node(str(id(fn)), get_fn_name(fn))
+            add_node_to_subgraphs(str(id(fn)), get_fn_name(fn), subgraphs=subgraphs)
 
         # recurse
         if hasattr(fn, 'next_functions'):
             for u in fn.next_functions:
                 if u[0] is not None:
                     dot.edge(str(id(u[0])), str(id(fn)))
-                    add_nodes(u[0])
+                    # add_nodes(u[0])
+                    add_nodes(u[0], copy(subgraphs))
 
     def add_base_tensor(var, color='darkolivegreen1'):
         if var in seen:
